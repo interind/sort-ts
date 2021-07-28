bd214d43305648187b84a1e4f19f4515e011253d
Aleksandr <interind@yandex.ru>
2021-07-28 19:37:37 +0300

    create classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello word');
+import { Sorter } from "./components/Sorter";
+import { NumbersCollection } from './components/NumbersCollection';
+import { CharactersCollection } from "./components/CharactersCollection";
+import { LinkedList } from './components/LinkedList';
+
+const arr = [3, -1, 5, 10, -8, 0, 1];
+const str = 'Lessing';
+const numberCollection = new NumbersCollection(arr);
+const charactersCollection = new CharactersCollection(str);
+const linkedList = new LinkedList();
+linkedList.add(-1);
+linkedList.add(100);
+linkedList.add(2);
+linkedList.add(56);
+numberCollection.sort();
+charactersCollection.sort();
+linkedList.sort();
+linkedList.print();
+numberCollection.print();
+charactersCollection.print();
+
