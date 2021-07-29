968b7dc80f9a75645d99edf647f2ee5403d24ebf
Aleksandr <interind@yandex.ru>
2021-07-29 17:19:20 +0300

    visible result

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 import { NumbersCollection } from './components/NumbersCollection';
 import { CharactersCollection } from "./components/CharactersCollection";
-import { LinkedList } from './components/LinkedList';
 
 const str = 'Lessing';
 const charactersCollection = new CharactersCollection(str);
-const linkedList = new LinkedList;
 const body: Element = document.body;
 const result: HTMLDivElement = document.createElement('div');
 result.classList.add('result');
+const label: HTMLLabelElement = document.createElement('label');
+label.classList.add('form__label');
+label.htmlFor = 'input';
+label.textContent = 'Введите числа через запятую для сортировки:';
 const form: HTMLFormElement = document.createElement('form');
 form.classList.add('form');
 const input: HTMLInputElement = document.createElement('input');
 input.classList.add('form__input');
+input.name = 'input';
 input.type = 'text';
+input.autocomplete = 'off';
+input.maxLength = 100;
+input.pattern = '[1-9,]+';
 const button: HTMLButtonElement = document.createElement('button');
 button.classList.add('form__submit');
 button.type = 'submit';
 
 
 button.textContent = 'Отправить'
+input.addEventListener('input', clearResult);
+form.appendChild(label);
 form.appendChild(input);
+form.appendChild(result);
 form.appendChild(button);
 form.addEventListener('submit', printResult);
 body.appendChild(form);
@@ -30,8 +39,16 @@ function printResult(evt: Event): void {
   const numberCollection = new NumbersCollection(arr);
   numberCollection.sort();
   result.textContent = `До: ${input.value} После: ${numberCollection.print()}`;
+  result.style.visibility = 'visible';
   form.reset();
-  body.appendChild(result);
+}
+
+function clearResult(evt: Event): void {
+  const target = evt.target instanceof HTMLInputElement ? evt.target : {value: null};
+  if (target.value) {
+    result.textContent = '';
+    result.style.visibility = 'hidden';
+  }
 }
 
 charactersCollection.sort();
